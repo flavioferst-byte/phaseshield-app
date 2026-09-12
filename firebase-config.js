7932dc3a6feb8137374a8dceafc61f68ef71dfe9
groupnuvion <groupnuvion@gmail.com>
2026-09-12 19:49:10 -0300

    fix: Enable same-domain authDomain cloakerblackvoice.com.br via native server proxy to eliminate cross-domain blank white popup issue

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 const FIREBASE_CONFIG = {
     apiKey:            "AIzaSyC-msdZclxoKC_xqM_mtPkNiRVg3mwbc4E",
-    authDomain:        "blackvoice-6d009.firebaseapp.com",
+    authDomain:        "cloakerblackvoice.com.br",
     projectId:         "blackvoice-6d009",
     storageBucket:     "blackvoice-6d009.firebasestorage.app",
     messagingSenderId: "751660110731",
