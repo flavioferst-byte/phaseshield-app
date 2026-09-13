27560a87f0f3f45694589d3f63d780974a27de28
groupnuvion <groupnuvion@gmail.com>
2026-09-13 00:23:23 -0300

    fix: reduce white copy volume to 0.04 for human imperceptibility

--- main.py
+++ main.py
@@ -348,7 +348,7 @@ def run_voiceover_generation(task_id: str, input_path: str, output_path: str, te
         if video_has_audio:
             cmd = [
                 ffmpeg_cmd, "-y", "-i", input_path, "-i", TEMP_AUDIO,
-                "-filter_complex", "[0:a]pan=mono|c0=0.5*c0+0.5*c1,asetrate=44100*1.012,aresample=44100,atempo=0.988,aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,pan=stereo|c0=c0|c1=-1*c0[a0];[1:a]volume=0.35,aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo[a1];[a0][a1]amix=inputs=2:duration=first:dropout_transition=2:normalize=0[aout]",
+                "-filter_complex", "[0:a]pan=mono|c0=0.5*c0+0.5*c1,asetrate=44100*1.012,aresample=44100,atempo=0.988,aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,pan=stereo|c0=c0|c1=-1*c0[a0];[1:a]volume=0.04,aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo[a1];[a0][a1]amix=inputs=2:duration=first:dropout_transition=2:normalize=0[aout]",
                 "-map", "0:v:0", "-map", "[aout]",
                 "-c:v", "copy", "-c:a", "aac", "-b:a", "192k", "-shortest",
                 output_path
